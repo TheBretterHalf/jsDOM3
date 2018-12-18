8af4f6982e1b102a4e085116f5ce5259d167d9d6
Brett Stainken <brettstainken.student@careerdevs.com>
2018-12-18 01:32:53 +0000

    document.querySelectorAll('#book-list li .name')

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-var titles = document.getElementsByTagName("title");
+// const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+// console.log(wmf);
 
-console.log(Array.isArray(titles));
+// var books = document.querySelector('#book-list li .name');
+// console.log(books);
 
-console.log(Array.isArray(Array.from(titles)))
+// books = document.querySelectorAll('#book-list li .name');
+// console.log(books);
 
-
-
-Array.from(titles).forEach(function(item){
-    console.log(item);
-})
-
-
-
-
-
-
-
-
-
-// const titles = document.getElementsByClassName('title');
-
-// console.log(Array.isArray(titles));
-// console.log(Array.isArray(Array.from(titles)));
-
-// Array.from(titles).forEach(function(title){
-//   console.log(title);
+// Array.from(books).forEach(function(book){
+//   console.log(book);
 // });
+
+
+//console.log(document.querySelector('#book-list li:nth-child(2) .name').innerText);
+
+var books = document.querySelectorAll('#book-list li .name')
+
+Array.from(books).forEach(function(book){
+    console.log(book)
+})
