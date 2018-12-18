a8a9c453d83918e1333686f28ff67cb033db37ba
Brett Stainken <brettstainken.student@careerdevs.com>
2018-12-18 23:09:09 +0000

    book.textContent+=' [Book Title]'

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
-// const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-// console.log(wmf);
-
-// var books = document.querySelector('#book-list li .name');
-// console.log(books);
-
-// books = document.querySelectorAll('#book-list li .name');
-// console.log(books);
-
-// Array.from(books).forEach(function(book){
-//   console.log(book);
-// });
-
-
-//console.log(document.querySelector('#book-list li:nth-child(2) .name').innerText);
-
-var books = document.querySelectorAll('#book-list li .name')
+var books = document.querySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function(book){
-    console.log(book)
-})
+   book.textContent+=' [Book Title]'
+});
+
+var bookList = document.querySelector('#book-list');
+bookList.innerHTML='<h2>Books and More Books</h2>'
+bookList.innerHTML+='<p>This is how you add HtmL</p>'
+
+
+
+// const books = document.querySelectorAll('#book-list li .name');
+
+// Array.from(books).forEach(function(book){
+//   book.textContent += ' (Book title)';
+// });
+
+// const bookList = document.querySelector('#book-list');
+// bookList.innerHTML = '<h2>Books and more books...</h2>';
+// bookList.innerHTML += '<p>This is how you add HTML content</p>';
