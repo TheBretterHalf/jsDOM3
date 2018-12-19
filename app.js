fb1dd5a493bdb0507b0f6ce12914c73a932ed6d8
Brett Stainken <brettstainken.student@careerdevs.com>
2018-12-19 04:47:13 +0000

    keyup, toLowerCase

--- app.js
+++ app.js
@@ -43,3 +43,17 @@ hidebox.addEventListener('change', function(e){
     list.style.display = "block"
   }
 })
+
+var searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+  var term = e.target.value.toLowerCase();
+  var books = list.getElementsByTagName('li')
+  Array.from(books).forEach(function(book){
+    var title = book.firstElementChild.textContent
+    if (title.toLowerCase().indexOf(term) != -1){
+      book.style.display = 'block';
+    } else{
+      book.style.display = 'none';
+    }
+  })
+})
